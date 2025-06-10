43e029947c7ad8d7f1dd3ad9d391046c40179b3a
Mateo Bedoya <mateo.bedoyao@udea.edu.co>
2025-06-10 11:54:21 -0500

    Organización de componentes y estilos

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "./Button";
+
+
+const SubHeader = () => {
+    return (
+        /* Sub-header con texto, botones e imagen */
+        <section className="flex flex-col md:flex-row bg-[#EEEEEE] gap-6 min-h-[250px]">
+            <div className="w-full md:w-[70%] flex flex-col justify-center p-6">
+                <h1 className="text-xl font-semibold mb-4">
+                    Bienvenido al sistema AHURA.
+                </h1>
+                <p className="mb-4">Gestión de activos del conocimiento.</p>
+                <div className="flex flex-wrap gap-2">
+                    <Button text={"Iniciar sesión"} type="primary" ></Button>
+                    <Button text={"Ver activos públicos"} type="secondary" ></Button>
+                    <Button text={"Conocer más sobre el pecet"} type="success" ></Button>
+                </div>
+            </div>
+            <div className="w-full md:w-[30%]">
+                <img
+                    src="/Semi_header_pecet.png"
+                    alt="Imagen descriptiva"
+                    className="w-full h-full object-cover rounded"
+                />
+            </div>
+        </section>
+    )
+}
+
+export default SubHeader
