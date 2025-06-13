09afd68379c96e1c2c0e2820f59ee8149aaf418f
matiusbedo <mateo.bedoyao@udea.edu.co>
2025-06-13 12:48:01 -0500

    Formulario de registro y maquetado para ver el detalle de un AC

--- src/components/organisms/MainFeatures.jsx
+++ src/components/organisms/MainFeatures.jsx
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
 import RegisterAC from "./RegisterAC";
+import ViewAC from "../organisms/ViewAC";
 import Button from "../atoms/Button";
 
 const MainFeatures = () => {
   const [showForm, setShowForm] = useState(false);
+  const [showExample, setShowExample] = useState(false);
+
+  const ejemploAC = {
+    id: "AC123",
+    titulo: "Método de recolección de zancudos",
+    descripcion: "Método utilizado en zonas rurales para capturar zancudos transmisores.",
+    fecha: "2024-05-12",
+    tipoActivo: "Físico",
+    tipoConocimiento: "Métodos desarrollados o mejorados",
+    formato: "PDF",
+    palabrasClave: "zancudos, recolección, método",
+    origen: "Investigación",
+    ubicacion: "Laboratorio PECET, Medellín",
+    accesible: "Se puede acceder",
+    clasificacion: "Alta",
+    autor: "Juan Pérez",
+    propietarioAC: "PECET",
+    visibilidad: "Público",
+    estadoAC: "Finalizado",
+    imagen: "/public/Prueba.jpg",
+  };
 
   const handleRegisterClick = () => {
     setShowForm(true);
+    setShowExample(false);
+  };
+
+  const handleSearchClick = () => {
+    setShowExample(true);
+    setShowForm(false);
   };
 
   return (
@@ -15,7 +43,7 @@ const MainFeatures = () => {
         ¿Qué puedes hacer aquí?
       </h3>
 
-      {!showForm ? (
+      {!showForm && !showExample ? (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <Button
             onClick={handleRegisterClick}
@@ -28,6 +56,7 @@ const MainFeatures = () => {
             type="light"
           />
           <Button
+            onClick={handleSearchClick}
             text="Buscar activos de conocimiento"
             type="secondary"
           />
@@ -36,12 +65,15 @@ const MainFeatures = () => {
             type="success"
           />
         </div>
-      ) : (
+      ) : showForm ? (
         <RegisterAC />
+      ) : (
+        <ViewAC ac={ejemploAC} />
       )}
     </main>
   );
 };
 
 export default MainFeatures;
+
 
