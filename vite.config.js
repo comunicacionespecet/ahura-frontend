ca419e7ff8d879c94356d667fad3cdd506332abb
matiusbedo <mateo.bedoyao@udea.edu.co>
2025-07-12 11:51:35 -0500

    Prueba de back en despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/assets': {
-                target: 'http://ec2-54-156-15-66.compute-1.amazonaws.com:3000',
-                changeOrigin: true,
-            },
-        },
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/assets': {
+        target: 'http://ec2-54-156-15-66.compute-1.amazonaws.com:3000',
+        changeOrigin: true,
+      },
     },
+  },
 });
